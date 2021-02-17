086e62b8c9f8058cecc46baf5e41fe8367a19ae8
Ivan Gusić <igusic00@fesb.hr>
2021-02-17 19:47:54 +0100

    Fixing Footer margin

---
+++ src/modules/NewsContent/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './style.module.css'
+ 
+const NewsContent = () => <section className={styles.NewsContent}>
+    <li className={styles.natjecaji}>
+{/*             <li className={styles.naslov1}>Natječaji</li>
+            <li className={styles.natjecajiparagraph1}>
+                <li className={styles.natjecajiparagraph1text}>Raspisan natječaj za dodjelu studentskih stipendija</li>
+            </li>
+            <li className={styles.natjecajiparagraph2}>
+                <li className={styles.natjecajiparagraph2text}>Potpore investicijskim projektima</li>
+            </li> */}
+    </li>
+</section> 
+ 
+export default NewsContent
--- src/pages/vijesti.js
+++ src/pages/vijesti.js
@@ -1,9 +1,10 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
+import NewsContent from "../modules/NewsContent"
  
 const NewsPage = () => (
     <HeaderFooterLayout activeTab="Vijesti">
-        
+        <NewsContent/>
     </HeaderFooterLayout>
 )
  
